50617fc33126511a5c4347b6309b5457a497cf81
fcretin <fcretin@student.42lyon.fr>
2025-04-04 16:30:47 +0200

    best road i think

--- src/utils/ft_clear.c
+++ src/utils/ft_clear.c
@@ -6,7 +6,7 @@
 /*   By: fcretin <fcretin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:17:02 by fcretin           #+#    #+#             */
-/*   Updated: 2025/04/04 14:59:15 by fcretin          ###   ########.fr       */
+/*   Updated: 2025/04/04 16:30:06 by fcretin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int	ft_clear(t_data *data, int return_v)
 
 	i = (int)data->arg.n_philo;
 	while (--i != -1)
-	{
 		pthread_mutex_destroy(&data->p[i].l_fork);
-		// pthread_mutex_destroy(&data->p[i].eat_lock);
-	}
 	pthread_mutex_destroy(&data->arg.eat_lock);
 	pthread_mutex_destroy(&data->arg.write_lock);
 	pthread_mutex_destroy(&data->arg.stop_sim);
